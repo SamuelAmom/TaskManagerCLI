c230e2886d4957c60b39e282c20f6cc9356022ce
SamuelAmom <samuel.amom01@gmail.com>
2025-03-26 01:40:40 -0300

    Adicionei a funcionalidade Getters e Setters

---
+++ TaskManagerCLI/src/Task.java
@@ -0,0 +1,32 @@
+import  java .time.LocalDateTime;
+
+//Atributos
+public class Task {
+    private int id;
+    private String descricao;
+    private  boolean concluida;
+    private LocalDateTime dataCriacao;
+
+    public Task (int id, String descricao){
+        this.id = id;
+        this.descricao = descricao;
+        this.concluida = false;
+        this.dataCriacao = LocalDateTime.now();
+    }
+
+    //Getters and Setters
+    public int getId(){return id;}
+    public String getDescricao(){return descricao;}
+    public boolean isConcluida(){return concluida;}
+    public LocalDateTime getDataCriacao (){return dataCriacao;}
+
+    public void setConcluida(boolean concluida){
+        this.concluida = concluida;
+    }
+
+    public String ToString (){
+        return "[" + (concluida ? "✔": "✘") + "]" + descricao + "(Criada em: " + dataCriacao + ")";
+    }
+}
+
+
